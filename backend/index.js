8e886f6fb42c16780e7de6a2f0420efe682a24ea
lucaslps01 <lucaslsantos159@gmail.com>
2023-02-07 17:33:29 -0300

    melhorias na autenticação de usuário

--- backend/index.js
+++ backend/index.js
@@ -7,29 +7,81 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const credentials = {
-    email: 'contato@type.dev.br',
-    senha: '123456'
+
+
+function getUser(email) {
+    const users = [
+        {
+            email: 'contato@type.dev.br',
+            senha: '123456',
+            role: 'admin'
+        },
+        {
+            email: 'contato@type.dev.br',
+            senha: '123456',
+            role: 'customer'
+        }
+    ]
+    return users.find(user => user.email === email)
+}
+
+function isaAuthenticated(req, resp, next) {
+
+
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        jwt.verify(token, key)
+        next()
+
+    } catch (e) {
+        return res.status(401).json({ error: 'Not authorized' })
+    }
+
+
 }
 
 app.post('/auth', (req, res) => {
     const { email, senha } = req.body;
-    if (email === credentials.email && senha === credentials.senha) {
-        return res.status(200).json({
-            
-            token: jwt.sign({ 
-                
 
-                email,
-                role: 'admin'
-            }, key)
+    const user = getUser(email)
+
+    if ( senha === user.senha) {
+        if (user.role !== 'admin'){
+            return res.status(403).send({
+                error: 'User not authorized'
+            })
+        }
+        return res.status(200).json({
+
+            token: jwt.sign(
+                {
+
+
+                    email,
+                    role: 'admin'
+                },
+                key,
+                { expiresIn: "24h" }
+
+            )
 
 
         })
     }
-    
+
     return res.status(401).json({
         Erro: 'nos dados de autenticação'
-     })
+    })
+})
+
+app.get('/users', isaAuthenticated, (req, res) => {
+
+    return res.status(200).json([{
+        email: 'contato@type.dev.br'
+
+
+    }])
+
+
 })
 app.listen(3000, () => console.log('Running on port 3000!'))
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,49 @@
-import { Login } from "./pages/Login"
-import { ListUsers } from "./pages/List.Users";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import { Login } from "./pages/Login";
+import { ListUsers } from "./pages/List.Users";
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode"
+
+function PrivateRoute({ children }) {
+  const token = Cookies.get('token')
+  if (token){
+    const payload = jwtDecode(token)
+    if (Date.now() < payload.exp * 1000){
+      return children
+    }
+    Cookie.remove('token')
+    
+  }
+  return <Navigate to='/' />
+  
+ }
+
+ function ProtectedRoute({ children }) {
+  const token = Cookies.get('token')
+  if (token){
+    return <Navigate to='/app' />
+  }
+  
+  return children
+ }
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Login/>,
-    },
-    {
-      path: "/app",
-      element: <ListUsers />
-    }
-  ]);
-export function App () {
-    return  <RouterProvider router={router} />
+  {
+    path: "/",
+    element: <ProtectedRoute>
+      <Login />
+      </ProtectedRoute>,
+  },
+  {
+    path: "/app",
+    element: (
+      <PrivateRoute>
+        <ListUsers />
+      </PrivateRoute>
+    ),
+  },
+]);
+export function App() {
+  return <RouterProvider router={router} />;
 }
