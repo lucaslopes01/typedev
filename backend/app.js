df2edb25fea3d75e2858a220172e2e5fb019bd57
lucaslps01 <lucaslsantos159@gmail.com>
2023-02-16 20:45:07 -0300

    create controllers

--- backend/app.js
+++ backend/app.js
@@ -1,74 +1,14 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
 import cors from 'cors'
 import { isaAuthenticated } from './middlewares/auth.middleware' 
- 
+ import { authenticate } from './controllers/auth.controller'
+import { getUser } from './controllers/users.controlle'
  
 
  export const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.post('/auth', authenticate )
 
-
-function getUser(email) {
-    const users = [
-        {
-            email: 'contato@type.dev.br',
-            senha: '123456',
-            role: 'admin'
-        },
-        {
-            email: 'rodrigo@type.dev.br',
-            senha: '123456',
-            role: 'customer'
-        }
-    ]
-    return users.find(user => user.email === email)
-}
-
-
-app.post('/auth', (req, res) => {
-    const { email, senha } = req.body;
-
-    const user = getUser(email)
-
-    if ( senha === user.senha) {
-        if (user.role !== 'admin'){
-            return res.status(403).send({
-                error: 'User not authorized'
-            })
-        }
-        return res.status(200).json({
-
-            token: jwt.sign(
-                {
-
-
-                    email,
-                    role: 'admin'
-                },
-                process.env.JWT_TOKEN ,
-                { expiresIn: "24h" }
-
-            )
-
-
-        })
-    }
-
-    return res.status(401).json({
-        error: 'Authentication data error'
-    })
-})
-
-app.get('/users', isaAuthenticated, (req, res) => {
-
-    return res.status(200).json([{
-        email: 'contato@type.dev.br'
-
-
-    }])
-
-
-})
+app.get('/users', isaAuthenticated, getUser)
---
+++ backend/controllers/users.controlle.js
@@ -0,0 +1,10 @@
+export function getUser (req, res)  {
+
+  return res.status(200).json([{
+      email: 'contato@type.dev.br'
+
+
+  }])
+
+
+}
